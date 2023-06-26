aac891fd8cc59651f95fe5f5e3c7234931f3b9c0
huizhongxue <hxue1@bu.edu>
2023-06-25 22:01:01 -0400

    canConstruct.py

--- canConstruct.py
+++ canConstruct.py
@@ -1,3 +1,4 @@
+# indicator function
 def canConstruct(phrase, list, memo = {}):
     if phrase in memo: return memo[phrase]
     if phrase == '': return True
@@ -11,10 +12,23 @@ def canConstruct(phrase, list, memo = {}):
     memo[phrase] = bool(I)
     return bool(I)
 
+# no indicator function
+def caanConstruct(phrase, list, memo = {}):
+    if phrase in memo: return memo[phrase]
+    if phrase == '': return True
+
+    for word in list:
+        if len(phrase) >= len(word) and phrase[:len(word)] == word:
+            if canConstruct(phrase[len(word):], list): 
+                memo[phrase] = True
+                return True
+    
+    memo[phrase] = False
+    return False
 
 
 
-print(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
-print(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'boar']))
-print(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
-print(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee']))
+print(caanConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']))
+print(caanConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'boar']))
+print(caanConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']))
+print(caanConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeef', ['e', 'ee', 'eee', 'eeee', 'eeeee', 'eeeeee']))
