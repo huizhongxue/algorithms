65bace690b4be99bb0ccdda91cb19e2b94d3739f
huizhongxue <hxue1@bu.edu>
2023-06-26 20:27:09 -0400

    longest common subsequence

--- lcs.py
+++ lcs.py
@@ -1,3 +1,20 @@
-def lcs(a):
+def lcs(a, b, memo = {}):
+    if (a, b) in memo: return memo[(a, b)]
+    if a == '' or b == '': return 0
 
-    return 0
+    m = 0
+
+    if a[0] == b[0]: 
+        m = 1 + lcs(a[1:], b[1:], memo)
+    else:
+        if lcs(a[1:], b, memo) > lcs(a, b[1:], memo):
+            m = lcs(a[1:], b, memo)
+        else:
+            m = lcs(a, b[1:], memo)
+
+    memo[(a, b)] = m
+    return m
+
+print(lcs("abcde", "ace", {}))
+print(lcs("abc", "abc", {}))
+print(lcs("abc", "def", {}))
